00d22851b94a7bf0399daa14430d6d1bac37b33f
Greivin Cruz <greivin.cruz@geoinn.onmicrosoft.com>
2022-03-15 08:11:39 -0600

    Primer release de app

---
+++ src/main/java/com/gestion/productos/GestionProductos1Application.java
@@ -0,0 +1,13 @@
+package com.gestion.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProductos1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProductos1Application.class, args);
+	}
+
+}
